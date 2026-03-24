9aa8c616509f88ea5d18d991c3ab6c3da14e278f
Meiich <mehdi97maleki@gmail.com>
2026-03-24 13:22:49 +0400

    Initial commit

---
+++ Weather API/weather.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_city():
+    name = input("Enter city name: ")
+    return name
+
+
+def get_weather(city):
+    
+
+    api_key = "2a66447393896a63060292271777ec9c"
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric")
+    return response.json()
+
+def show_weather(data):
+    print("City: ", data["name"])
+    print("temperature: ", data["main"]["temp"], "°C")
+    print("Description: ", data["weather"][0]["description"])
+    print("Humidity: ", data["main"]["humidity"], " %")
+
+def main():
+    name = get_city()
+    data = get_weather(name)
+    show_weather(data)
+
+main()

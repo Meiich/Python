52238bec286813d9788303b5c7491a5e658e6a39
Meiich <mehdi97maleki@gmail.com>
2026-03-24 14:45:31 +0400

    fix API key errors

--- Weather API/weather.py
+++ Weather API/weather.py
@@ -1,8 +1,10 @@
+import os
 import requests
 from dotenv import load_dotenv
-import os
+
 load_dotenv()
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
 
 def get_city():
     name = input("Enter city name: ")
@@ -10,21 +12,28 @@ def get_city():
 
 
 def get_weather(city):
-    
-
-    api_key = API_KEY
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric")
+    response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    )
     return response.json()
 
+
 def show_weather(data):
-    print("City: ", data["name"])
-    print("temperature: ", data["main"]["temp"], "°C")
-    print("Description: ", data["weather"][0]["description"])
-    print("Humidity: ", data["main"]["humidity"], " %")
+    if data.get("cod") != 200:
+        print("Error:", data.get("message", "Something went wrong"))
+        return
+
+    print("City:", data["name"])
+    print("Temperature:", data["main"]["temp"], "°C")
+    print("Description:", data["weather"][0]["description"])
+    print("Humidity:", data["main"]["humidity"], "%")
+
 
 def main():
-    name = get_city()
-    data = get_weather(name)
+    city = get_city()
+    data = get_weather(city)
     show_weather(data)
 
-main()
+
+if __name__ == "__main__":
+    main()
